9173ba6d8fb374e3e27fde5f7060dd5e315fc366
Dan N <dn691997@gmail.com>
2022-03-21 20:25:21 -0700

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require("fs");
+const generatePage = require("./src/page-template");
 
-const printProfileData = (profileDataArr) => {
-	profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+	if (err) throw err;
+	console.log("Portfolio complete! Check out index.html to see the output");
+});
+
+// const printProfileData = (profileDataArr) => {
+// 	profileDataArgs.forEach((profileItem) => console.log(profileItem))}
+// printProfileData(profileDataArgs)
+
+//array
+// const named = profileDataArgs[0];
+// const github = profileDataArgs[1];
